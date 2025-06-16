e1c49b26fb7ecfdfb9f38aadf97d5607fe634c14
huijingj <vanessajin0426@gmail.com>
2025-06-16 15:46:29 -0700

    1 - Working on signup route (todo: send welcome email)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,62 @@
-export const signup = (req, res) => {
-    res.send("signup")
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+    try {
+        const { name, username, email, password } = req.body;
+
+        if (!name || !username || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const existingEmail = await User.findOne({ email })
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const existingUsername = await User.findOne({ username })
+        if (existingUsername) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            username,
+        })
+
+        await user.save();
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "3d"
+        });
+
+        res.cookie("jwt-linkedin", token, {
+            httpOnly: true, // prevent XSS attacks
+            maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+            sameSite: "strict", // prevent CSRF attacks
+            secure: process.env.NODE_ENV === "production" // prevents man-in-the-middle attacks
+        })
+
+        res.status(201).json({
+            message: "User created successfully"
+        })
+
+        // todo: send welcome email
+
+    } catch (error) {
+        console.log("Error in signup:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // parse JSON req.body
+
 app.use("/api/v1/auth", authRoutes)
 
 app.listen(PORT, () => {
