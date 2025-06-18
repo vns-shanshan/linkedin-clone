2a85f47c28428624b0ea98a48998eac77bbcfd40
huijingj <vanessajin0426@gmail.com>
2025-06-18 15:40:48 -0700

    2 - Frontend setup, initialize cors in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +15,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use(express.json({ limit: "5mb" })); // parse JSON req.body
 app.use(cookieParser())
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/layout/Layout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/auth/SignUpPage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
